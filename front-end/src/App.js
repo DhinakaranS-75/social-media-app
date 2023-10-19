f1d2eee6e31fed83064d501ddbe865e4840cfc32
DhinakaranS-75 <dhinakaran75493@gmail.com>
2023-10-20 00:07:23 +0530

    nav bar code

--- front-end/src/App.js
+++ front-end/src/App.js
@@ -6,8 +6,15 @@ import Register from "./pages/register/Register";
 import Home from "./pages/home/Home";
 import Profile from "./pages/profile/Profile";
 
-import { createBrowserRouter, Outlet, RouterProvider } from "react-router-dom";
+import {
+  createBrowserRouter,
+  Outlet,
+  Navigate,
+  RouterProvider,
+} from "react-router-dom";
 function App() {
+  const currentUser = true;
+
   const Layout = () => {
     return (
       <div>
@@ -21,10 +28,21 @@ function App() {
     );
   };
 
+  const ProtectedRoute = ({ children }) => {
+    if (!currentUser) {
+      return <Navigate to="/login" />;
+    }
+    return children;
+  };
+
   const router = createBrowserRouter([
     {
       path: "/",
-      element: <Layout />,
+      element: (
+        <ProtectedRoute>
+          <Layout />
+        </ProtectedRoute>
+      ),
       children: [
         {
           path: "/",
