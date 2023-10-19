40bd7366661b3f9ec3912c2db5d897d8b5f57c50
DhinakaranS-75 <dhinakaran75493@gmail.com>
2023-10-19 22:34:30 +0530

    new code

---
+++ front-end/src/pages/login/Login.jsx
@@ -0,0 +1,28 @@
+import "./login.scss";
+
+const Login = () => {
+  return (
+    <div className="login">
+      <div className="card">
+        <div className="left">
+          <h1>Hello Users</h1>
+          {/* <p>
+            welcome to My Social media website, Upload Your Picture & videos,
+            Comments{" "}
+          </p> */}
+          <p>
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Quod quia
+            nesciunt laudantium excepturi saepe ex natus, reiciendis architecto
+            nihil eum? Asperiores quis corporis sint expedita enim porro illo
+            animi dolor?
+          </p>
+          <span>Don't you have an account?</span>
+          <button>Register</button>
+        </div>
+        <div className="right"></div>
+      </div>
+    </div>
+  );
+};
+
+export default Login;
