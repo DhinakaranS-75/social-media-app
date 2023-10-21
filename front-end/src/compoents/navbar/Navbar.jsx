26c93024f10b97de04978b18feb1620075926102
DhinakaranS-75 <dhinakaran75493@gmail.com>
2023-10-21 11:47:28 +0530

    navbar

--- front-end/src/compoents/navbar/Navbar.jsx
+++ front-end/src/compoents/navbar/Navbar.jsx
@@ -8,9 +8,33 @@ import NotificationsOutlinedIcon from "@mui/icons-material/NotificationsOutlined
 import EmailOutlinedIcon from "@mui/icons-material/EmailOutlined";
 import PersonOutlinedIcon from "@mui/icons-material/PersonOutlined";
 import SearchOutlinedIcon from "@mui/icons-material/SearchOutlined";
+import { Link } from "@mui/material";
 
 const Navbar = () => {
-  return <div>Navbar</div>;
+  return (
+    <div className="navbar">
+      <div className="left">
+        <Link to="/" style={{ textDecoration: "none" }} />
+        <span>Socialmedia</span>
+        <HomeOutlinedIcon />
+        <DarkModeOutlinedIcon />
+        <GridViewOutlinedIcon />
+        <div className="search">
+          <SearchOutlinedIcon />
+          <input type="text" placeholder="Search " />
+        </div>
+      </div>
+      <div className="right">
+        <PersonOutlinedIcon />
+        <EmailOutlinedIcon />
+        <NotificationsOutlinedIcon />
+        <div className="user">
+          <img src="https://images.pexels.com/photos/1624496/pexels-photo-1624496.jpeg?auto=compress&cs=tinysrgb&w=600" alt="" />
+          <span>John Doe</span>
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default Navbar;
